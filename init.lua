74aa7dfed31938db25180fc07b21a4467f88b076
Kraust <secretdragoon@gmail.com>
2025-08-20 01:41:54 +0000

    Change my mind, gitlab is kinda nice.

--- init.lua
+++ init.lua
@@ -37,10 +37,11 @@ vim.pack.add({
     "https://github.com/rachartier/tiny-inline-diagnostic.nvim",
     "https://github.com/brianaung/compl.nvim",
     "https://github.com/alex-popov-tech/store.nvim",
-    "https://github.com/Mitch1000/backpack.nvim",
+    "https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git",
 
     -- Colorscheme
     "https://github.com/vague2k/vague.nvim",
+    "https://github.com/Mitch1000/backpack.nvim",
 }, {
     confirm = false,
 })
@@ -381,6 +382,62 @@ require("tiny-inline-diagnostic").setup({
         },
     },
 })
+
+require('gitlab').setup({
+    statusline = {
+        enabled = true
+    },
+    code_suggestions = {
+        -- For the full list of default languages, see the 'auto_filetypes' array in
+        -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim/-/blob/main/lua/gitlab/config/defaults.lua
+        auto_filetypes = {
+            'c',               -- C
+            'cpp',             -- C++
+            'csharp',          -- C#
+            'go',              -- Golang
+            'java',            -- Java
+            'javascript',      -- JavaScript
+            'javascriptreact', -- JavaScript React
+            'kotlin',          -- Kotlin
+            'markdown',        -- Markdown
+            'objective-c',     -- Objective-C
+            'objective-cpp',   -- Objective-C++
+            'php',             -- PHP
+            'python',          -- Python
+            'ruby',            -- Ruby
+            'rust',            -- Rust
+            'scala',           -- Scala
+            'sql',             -- SQL
+            'swift',           -- Swift
+            'terraform',       -- Terraform
+            'typescript',      -- TypeScript
+            'typescriptreact', -- TypeScript React
+            'sh',              -- Shell scripts
+            'html',            -- HTML
+            'css',             -- CSS
+        },
+        lsp_binary_path = 'node',
+        ghost_text = {
+            enabled = true,
+            toggle_enabled = nil,
+            accept_suggestion = "<leader><Tab>",
+            clear_suggestions = "<leader><S-Tab>",
+            stream = false,
+        },
+    },
+    language_server = {
+        workspace_settings = {
+            codeCompletion = {
+                enableSecretRedaction = true,
+            },
+            telemetry = {
+                enabled = false,
+                trackingUrl = nil,
+            },
+        },
+    },
+})
+
 
 -- Setup completion plugin with custom configuration
 require("compl").setup({
